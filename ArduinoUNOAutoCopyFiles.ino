f0609b655af7670e19336c7eb77d6b1700ede858
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-08-26 19:25:52 +0200

    Update ArduinoUNOAutoCopyFiles.ino

--- ArduinoUNOAutoCopyFiles.ino
+++ ArduinoUNOAutoCopyFiles.ino
@@ -1,12 +1,16 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int originalCardChipSelect = 5; // CS pin pre ORIGINAL SD kartu
-const int targetCardChipSelect = 10;  // CS pin pre TARGET SD kartu
-const int externalLedPin = 3; // Externý diódový pin
+const int originalCardChipSelect = 5; // CS pin pre pôvodnú SD kartu
+const int targetCardChipSelect = 10;  // CS pin pre cieľovú SD kartu
+const int externalLedPin = 3; // Externý diódový pin pre úspešné kopírovanie
+const int emptyCardLedPin = 4; // Oranžová diódová pin pre prázdnu SD kartu
+const int initErrorLedPin = 2; // Červená diódová pin pre zlyhanú inicializáciu SD karty
 
 void setup() {
   pinMode(externalLedPin, OUTPUT); // Nastavte externý diódový pin ako výstup
+  pinMode(emptyCardLedPin, OUTPUT); // Nastavte diódový pin pre prázdnu kartu ako výstup
+  pinMode(initErrorLedPin, OUTPUT); // Nastavte diódový pin pre zlyhanú inicializáciu ako výstup
   
   Serial.begin(9600);
   while (!Serial) {}
@@ -19,16 +23,30 @@ void setup() {
 
   Serial.println("Pôvodná SD karta inicializovaná.");
 
-  File root = SD.open("/");
-  while (true) {
-    File entry = root.openNextFile();
-    if (!entry) {
-      break;
+  File targetRoot = SD.open("/");
+  if (targetRoot) {
+    bool targetSDEmpty = true;
+    while (true) {
+      File entry = targetRoot.openNextFile();
+      if (!entry) {
+        break;
+      }
+      if (!entry.isDirectory()) {
+        copyFile(entry.name());
+        targetSDEmpty = false;
+      }
+      entry.close();
     }
-    if (!entry.isDirectory()) {
-      copyFile(entry.name());
+    
+    if (targetSDEmpty) {
+      Serial.println("TargetSD-EMPTY!");
+      digitalWrite(emptyCardLedPin, HIGH); // Rozsvieti oranžovú LED diódu pre prázdnu kartu
     }
-    entry.close();
+    
+    targetRoot.close();
+  } else {
+    Serial.println("Nepodarilo sa otvoriť priečinok na cieľovej SD karte.");
+    digitalWrite(initErrorLedPin, HIGH); // Rozsvieti červenú LED diódu pre zlyhanú inicializáciu SD karty
   }
   
   // Blikanie externej LED diódy 10 krát
@@ -47,25 +65,25 @@ void loop() {
 }
 
 void copyFile(const char* filename) {
-  File originalFile = SD.open(filename);
-  if (!originalFile) {
-    Serial.println("Nepodarilo sa otvoriť pôvodný súbor.");
-    return;
-  }
-
-  File targetFile = SD.open(filename, FILE_WRITE);
+  File targetFile = SD.open(filename);
   if (!targetFile) {
     Serial.println("Nepodarilo sa otvoriť cieľový súbor.");
-    originalFile.close();
     return;
   }
 
-  while (originalFile.available()) {
-    targetFile.write(originalFile.read());
+  File originalFile = SD.open(filename, FILE_WRITE);
+  if (!originalFile) {
+    Serial.println("Nepodarilo sa otvoriť pôvodný súbor.");
+    targetFile.close();
+    return;
   }
 
-  originalFile.close();
+  while (targetFile.available()) {
+    originalFile.write(targetFile.read());
+  }
+
   targetFile.close();
+  originalFile.close();
 
   Serial.println("Súbor bol úspešne skopírovaný.");
 }
